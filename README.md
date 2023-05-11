4f1ffa197a8e3f001577708bf2e615fb6b6b1585
dosisido <dosisido46@gmail.com>
2023-05-11 16:51:39 +0200

    Made the script more general

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 1. create virtual environment `python -m venv .venv`
 2. move to the .venv environment `.venv/scripts/activate`
-3. update pip `python -m pip install --update pip`
+3. update pip `python -m pip install --upgrade pip`
 4. install requirements `pip install -r requirements.txt`
-5. modify file [lectures.txt](/lectures.txt)
-6. run [main.py](/main.py) and enjoy
+5. run [main.py](/main.py) and enjoy
+
+## About links.txt
+The file is not included, but if you run [main.py](/main.py) the first time it autmatically
+creates it for you.  
+Then you need to insert one lin for line and it's done!
--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 import requests
 import os
 
-FILE = "lectures.txt"
-DIR = "lectures"
+FILE = "links.txt"
+DIR = "download"
 
 def lectures_downloader(name, url):
-    ext = url.strip()[::-1].split('.', 1)[0][::-1]
-    
+    # ext = url.strip()[::-1].split('.', 1)[0][::-1]
+
     r = requests.get(url, allow_redirects=True)
-    if 'video' not in r.headers.get('content-type'): 
-        print("Not video type")
-        
-    if ext != r.headers.get('content-type').replace('video/', ''):
-        print("Extension error")
+
+    content_type, ext = r.headers.get('content-type').strip().split('/')
+    print(f"content_type: {content_type}; extension : {ext}")
 
     path = os.path.join(DIR, f"{name}.{ext}")
     open(path, 'wb').write(r.content)
@@ -22,6 +20,13 @@ def lectures_downloader(name, url):
 
 
 def main():
+
+    if not os.path.exists(FILE):
+        open(FILE, 'w').close()
+        print(f"Created {FILE} file")
+        print(f"Please fill {FILE} file with links")
+        exit(0)
+
 
     if not os.path.exists(DIR):
         os.makedirs(DIR)
